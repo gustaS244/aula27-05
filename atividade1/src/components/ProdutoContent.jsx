4c4e89d082eb38b557c153d55b71b1c068bc5a6b
Mona Lis$ git config --global user.name Mona <nepomucenagustavo@gmail.com>
2024-06-17 16:46:26 -0300

    .

--- atividade1/src/components/ProdutoContent.jsx
+++ atividade1/src/components/ProdutoContent.jsx
@@ -7,8 +7,8 @@ export default function ProdutoContent() {
       <div className={styles.imgC}>
         <img src="https://pbs.twimg.com/profile_images/1494046092983517186/sfPdqJJS_400x400.jpg" alt="" />
         <img src="https://pbs.twimg.com/profile_images/1610453099323301892/RwtK8QDy_400x400.jpg" alt="" />
-        <img src="https://pbs.twimg.com/profile_images/1641391692652617729/9eom6BgG_400x400.jpg" alt="" />
         <img src="https://pbs.twimg.com/media/FhnFE_yXgAEMI8r.jpg" alt="" />
+        <img src="https://pbs.twimg.com/profile_images/1641391692652617729/9eom6BgG_400x400.jpg" alt="" />
       </div>
       <div className={styles.CapS}>
         <h2>Capivara Estudante R$500  </h2>
